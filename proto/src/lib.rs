54f59b3555365c7ff97db776dd5b9b072658f93f
Conor Griffin <conor.griffin37@mail.dcu.ie>
2017-10-06 20:56:28 +0100

    Change Rust GRPC implementation

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -1,6 +1,7 @@
-extern crate grpcio;
+extern crate grpc;
+extern crate grpc_rust;
 extern crate protobuf;
-extern crate futures;
+extern crate tls_api;
 
 pub mod mapreduce;
 pub mod mapreduce_grpc;
