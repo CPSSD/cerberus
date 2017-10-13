f8d1db7298ca781959dfbf2dcdd46ffb5ba254c6
Terry Bolt <bolt.terry@gmail.com>
2017-10-13 15:33:20 +0100

    Add FinalOutputObject.
    FinalOutputObject is a struct which, when serialised to JSON, complies
    with the libcerberus input/output API.

--- libcerberus/src/serialise.rs
+++ libcerberus/src/serialise.rs
@@ -15,11 +15,20 @@ pub struct IntermediateOutputObject<K: Serialize, V: Serialize> {
     pub pairs: Vec<IntermediateOutputPair<K, V>>,
 }
 
+/// `FinalOutputObject` is a struct comprising a collection of serialisable values representing the
+/// entire output of a reduce operation, ready to be serialised to JSON.
+#[derive(Serialize)]
+pub struct FinalOutputObject<V: Serialize> {
+    pub values: Vec<V>,
+}
+
 #[cfg(test)]
 mod tests {
     use serde_json;
     use super::*;
 
+    // Test that the JSON serialisation of IntermediateOutputObject matches the libcerberus JSON
+    // API.
     #[test]
     fn intermediate_output_object_json_format() {
         let output = IntermediateOutputObject {
@@ -41,5 +50,16 @@ mod tests {
         let json_string = serde_json::to_string(&output).unwrap();
 
         assert_eq!(expected_json_string, json_string)
+    }
+
+    // Test that the JSON serialisation of FinalOutputObject matches the libcerberus JSON API.
+    #[test]
+    fn final_output_object_json_format() {
+        let output = FinalOutputObject { values: vec!["barbaz", "bazbar"] };
+        let expected_json_string = "{\"values\":[\"barbaz\",\"bazbar\"]}";
+
+        let json_string = serde_json::to_string(&output).unwrap();
+
+        assert_eq!(expected_json_string, json_string);
     }
 }
