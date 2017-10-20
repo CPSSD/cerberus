e97c8d53a8031e2bbd79195a41b6bc4ee88c9a4c
Terry Bolt <bolt.terry@gmail.com>
2017-10-20 16:42:49 +0100

    Add map-reduce payload for the sprint one demo.

---
+++ libcerberus/src/bin/sprint-one-demo.rs
@@ -0,0 +1,65 @@
+extern crate cerberus;
+extern crate env_logger;
+#[macro_use]
+extern crate error_chain;
+
+use cerberus::*;
+use std::str::FromStr;
+
+struct WordCountMapper;
+impl Map for WordCountMapper {
+    type Key = String;
+    type Value = u8;
+    fn map<E>(&self, input: MapInputKV, mut emitter: E) -> Result<()>
+    where
+        E: EmitIntermediate<Self::Key, Self::Value>,
+    {
+        for token in input.value.split(char::is_whitespace) {
+            if !token.is_empty() {
+                emitter.emit(token.to_owned(), 1).chain_err(
+                    || "Error emitting map key-value pair.",
+                )?;
+            }
+        }
+        Ok(())
+    }
+}
+
+struct WordCountReducer;
+impl Reduce for WordCountReducer {
+    type Value = u64;
+    fn reduce<E>(&self, input: ReduceInputKV, mut emitter: E) -> Result<()>
+    where
+        E: EmitFinal<Self::Value>,
+    {
+        let mut total: u64 = 0;
+        for val in &input.values {
+            let int_val = u64::from_str(val).chain_err(|| {
+                format!("Unable to convert value {} to string.", val)
+            })?;
+            total += int_val;
+        }
+        emitter.emit(total).chain_err(|| {
+            format!("Error emitting value {:?}.", total)
+        })?;
+        Ok(())
+    }
+}
+
+fn run() -> Result<()> {
+    env_logger::init().chain_err(
+        || "Failed to initialise logging.",
+    )?;
+
+    let wc_mapper = WordCountMapper;
+    let wc_reducer = WordCountReducer;
+
+    let matches = cerberus::parse_command_line();
+    let registry = cerberus::register_mapper_reducer(&wc_mapper, &wc_reducer);
+
+    cerberus::run(&matches, &registry)
+}
+
+// Macro to generate a quick error_chain main function.
+// https://github.com/rust-lang-nursery/error-chain/blob/master/examples/quickstart.rs
+quick_main!(run);
