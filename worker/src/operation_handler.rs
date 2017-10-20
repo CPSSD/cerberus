093a7a81903479a3be41b68c9119303e96322012
Darragh Griffin <darragh.griffin24@mail.dcu.ie>
2017-10-20 23:42:26 +0100

    Refactor map operation thread

--- worker/src/operation_handler.rs
+++ worker/src/operation_handler.rs
@@ -164,101 +164,41 @@ fn log_reduce_operation_err(
     );
 }
 
-fn try_log_map_operation_err<T>(
-    res: &Result<T>,
-    worker_status_arc: &Arc<Mutex<WorkerStatusResponse_WorkerStatus>>,
-    operation_status_arc: &Arc<Mutex<WorkerStatusResponse_OperationStatus>>,
-) -> bool {
-    if let Err(ref err) = *res {
-        log_map_operation_err(err, worker_status_arc, operation_status_arc);
-        true
-    } else {
-        false
-    }
-}
-
 fn map_operation_thread_impl(
-    map_options: &PerformMapRequest,
     map_input_value: &str,
     mut child: Child,
-    worker_status_arc: Arc<Mutex<WorkerStatusResponse_WorkerStatus>>,
-    operation_status_arc: Arc<Mutex<WorkerStatusResponse_OperationStatus>>,
     map_result_arc: Arc<Mutex<Option<MapResponse>>>,
-) {
-    let json_input =
-        format!(
-        "{{ \"key\": {}, \"value\": {} }}",
-        map_options.get_input_file_path(),
-        map_input_value,
-    );
-
+) -> Result<()> {
     if let Some(stdin) = child.stdin.as_mut() {
-        let write_result = stdin.write_all(json_input.as_bytes()).chain_err(
+        stdin.write_all(map_input_value.as_bytes()).chain_err(
             || "Error writing to payload stdin.",
-        );
-
-        if try_log_map_operation_err(&write_result, &worker_status_arc, &operation_status_arc) {
-            return;
-        }
+        )?;
     } else {
-        self::set_failed_status(worker_status_arc, operation_status_arc);
-        error!("Error accessing stdin of payload binary.");
-        return;
+        return Err("Error accessing stdin of payload binary.".into());
     }
 
-    let output_result = child.wait_with_output().chain_err(
+    let output = child.wait_with_output().chain_err(
         || "Error waiting for payload result.",
-    );
+    )?;
 
-    let output = {
-        match output_result {
-            Ok(output) => output,
-            Err(ref err) => {
-                log_map_operation_err(err, &worker_status_arc, &operation_status_arc);
-                return;
-            }
-        }
-    };
+    let output_str = String::from_utf8(output.stdout).chain_err(
+        || "Error accessing payload output.",
+    )?;
 
-    let output_str_res =
-        String::from_utf8(output.stdout).chain_err(|| "Error accessing payload output.");
-
-    let output_str = {
-        match output_str_res {
-            Ok(output_str) => output_str,
-            Err(ref err) => {
-                log_map_operation_err(err, &worker_status_arc, &operation_status_arc);
-                return;
-            }
-        }
-    };
-
-    let map_results_res = parse_map_results(&output_str).chain_err(|| "Error parsing map results.");
-
-    let map_results = {
-        match map_results_res {
-            Ok(map_results) => map_results,
-            Err(ref err) => {
-                log_map_operation_err(err, &worker_status_arc, &operation_status_arc);
-                return;
-            }
-        }
-    };
+    let map_results = parse_map_results(&output_str).chain_err(
+        || "Error parsing map results.",
+    )?;
 
     let mut response = MapResponse::new();
     response.set_status(OperationStatus::SUCCESS);
     response.set_map_results(RepeatedField::from_vec(map_results));
 
-    let map_result_res = map_result_arc.lock();
-    match map_result_res {
+    match map_result_arc.lock() {
         Ok(mut map_result) => {
             *map_result = Some(response);
-            self::set_complete_status(worker_status_arc, operation_status_arc);
+            Ok(())
         }
-        Err(_) => {
-            self::set_failed_status(worker_status_arc, operation_status_arc);
-            error!("Map operation error: Could not write to map_result_arc.");
-        }
+        Err(_) => Err("Could not write to map_result_arc.".into()),
     }
 }
 
@@ -379,15 +319,19 @@ impl OperationHandler {
         let worker_status_arc = Arc::clone(&self.worker_status);
         let map_result_arc = Arc::clone(&self.map_result);
 
+        let map_input =
+            format!(
+            "{{ \"key\": {}, \"value\": {} }}",
+            map_options.get_input_file_path(),
+            map_input_value,
+        );
+
         thread::spawn(move || {
-            map_operation_thread_impl(
-                &map_options,
-                &map_input_value,
-                child,
-                worker_status_arc,
-                operation_status_arc,
-                map_result_arc,
-            );
+            let result = map_operation_thread_impl(&map_input, child, map_result_arc);
+            match result {
+                Ok(_) => self::set_complete_status(worker_status_arc, operation_status_arc),
+                Err(err) => log_map_operation_err(&err, &worker_status_arc, &operation_status_arc),
+            }
         });
 
         Ok(())
