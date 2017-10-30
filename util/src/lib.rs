407c0ed3f43f19ae460165805de0cd3bf9f6c058
Terry Bolt <bolt.terry@gmail.com>
2017-10-30 14:57:13 +0000

    Add custom formatting to util::init_logger.
    This includes the current time in the format HH:MM:SS.mmm.

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,3 +1,4 @@
+extern crate chrono;
 extern crate env_logger;
 #[macro_use]
 extern crate error_chain;
--- util/src/logging.rs
+++ util/src/logging.rs
@@ -1,22 +1,34 @@
+use std::env;
+
+use chrono::Utc;
 use env_logger::LogBuilder;
 use error_chain::ChainedError;
+use log::LogRecord;
+
 use errors::*;
-use std::env;
 
 // This makes the default logging level info for everything but the grpc modules which will use
 // error. This is because grpc modules output an unnecessary level of logging for info.
 const DEFAULT_LOG_CONFIG: &str = "info,grpc=error";
 
 pub fn init_logger() -> Result<()> {
-    let builder = &mut LogBuilder::new();
-    let builder = {
-        match env::var("RUST_LOG") {
-            Ok(log_config) => builder.parse(&log_config),
-            Err(_) => builder.parse(DEFAULT_LOG_CONFIG),
-        }
+    let format = |record: &LogRecord| {
+        format!(
+            "{}:{}:{}: {}",
+            record.level(),
+            record.location().module_path(),
+            Utc::now().format("%T%.3f"),
+            record.args()
+        )
     };
+    let log_config = env::var("RUST_LOG").unwrap_or_else(|_| DEFAULT_LOG_CONFIG.to_owned());
 
-    builder.init().chain_err(|| "Failed to build env_logger")?;
+    LogBuilder::new()
+        .format(format)
+        .parse(log_config.as_str())
+        .init()
+        .chain_err(|| "Failed to build env_logger")?;
+
     Ok(())
 }
 
