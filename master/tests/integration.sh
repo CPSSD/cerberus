689e6ed931dccbbdef17c197994612b6c4b1a354
Terry Bolt <bolt.terry@gmail.com>
2017-11-21 10:00:31 +0000

    Remove master integration test.
    The integration test fails if the master tests are run from a clean
    build environment. The shell script has been left in since it can be
    repurposed, but it no longer runs automatically as part of `cargo test`.
    Also fix errors in the shell script and set it to fail on error.

--- master/tests/integration.sh
+++ master/tests/integration.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+set -e
+
 cd ../target/debug
 
 # Setup directories.
@@ -15,16 +18,16 @@ master_pid=$!
 worker_pids=(0 0 0)
 for i in ${!worker_pids[*]}
 do
-	echo "Launching worker $i"
-	./worker -m [::]:10008 > integration-test/logs/worker-${i}.log 2>&1 &
-	worker_pids[$i]=$!
+    echo "Launching worker $i"
+    ./worker -m "[::]:10008" > integration-test/logs/worker-"${i}".log 2>&1 &
+    worker_pids[$i]=$!
 done
 
 echo ${worker_pids[*]}
 
 # Create some sample data.
 echo "how much wood would a wood chuck chuck if a wood chuck could chuck wood" \
-	> integration-test/input/sample.txt
+    > integration-test/input/sample.txt
 
 declare -A results
 results+=( [how]=1 [much]=1 [wood]=4 [would]=1 [a]=2 [chuck]=4 [could]=1 [if]=1 )
@@ -33,36 +36,36 @@ results+=( [how]=1 [much]=1 [wood]=4 [would]=1 [a]=2 [chuck]=4 [could]=1 [if]=1 
 sleep 1
 
 # Launch a MapReduce using the above data.
-./cli -m [::]:10008 run \
-	-b $PWD/examples/word-counter \
-	-i $PWD/integration-test/input \
-	-o $PWD/integration-test/output
+./cli -m "[::]:10008" run \
+    -b "$PWD"/examples/word-counter \
+    -i "$PWD"/integration-test/input \
+    -o "$PWD"/integration-test/output
 
-./cli -m [::]:10008 cluster_status
+./cli -m "[::]:10008" cluster_status
 
 # Launch the CLI to monitor status.
 echo "Launching CLI."
 while true
 do
-	done_status=`./cli -m [::]:10008 status 2>&1 | tee | grep "DONE" -o`;
-	fail_status=`./cli -m [::]:10008 status 2>&1 | tee | grep "FAILED" -o`;
-	if [ "$done_status" == "DONE" ]; then break; fi
-	if [ "$fail_status" == "FAILED" ]; then exit 1; fi
+    done_status=$(./cli -m "[::]:10008" status 2>&1 | tee | grep "DONE" -o);
+    fail_status=$(./cli -m "[::]:10008" status 2>&1 | tee | grep "FAILED" -o);
+    if [ "$done_status" == "DONE" ]; then break; fi
+    if [ "$fail_status" == "FAILED" ]; then exit 1; fi
 done
 
 # Kill any spawned processes.
 kill -9 ${master_pid} ${worker_pids[*]}
 
 # Verify that the output is correct.
-for key in ${!results[@]}
+for key in "${!results[@]}"
 do
-	count=`grep "[0-9]*" -o integration-test/output/${key}`
-	echo ${key} ${count}
-	if [ "${count}" != "${results[${key}]}" ]
-	then
-		echo "Error. Expected ${key}=${results[${key}]}, but got: $count"
-		exit 1
-	fi
+    count=$(grep "[0-9]*" -o integration-test/output/"${key}")
+    echo "${key}" "${count}"
+    if [ "${count}" != "${results[${key}]}" ]
+    then
+        echo "Error. Expected ${key}=${results[${key}]}, but got: $count"
+        exit 1
+    fi
 done
 
 # Cleanup.
