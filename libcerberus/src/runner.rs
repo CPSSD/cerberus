cb818c2d0b971e17e74532c7a69f8ff7e7d4b205
Darragh Griffin <darragh.griffin24@mail.dcu.ie>
2018-03-16 22:38:16 +0000

    Add internal combine for results of map operation

--- libcerberus/src/runner.rs
+++ libcerberus/src/runner.rs
@@ -1,14 +1,17 @@
+use std::collections::HashMap;
 use std::io::{stdin, stdout};
 
 use chrono::prelude::*;
 use clap::{App, ArgMatches, SubCommand};
 use serde::Serialize;
 use serde::de::DeserializeOwned;
+use serde_json;
 use uuid::Uuid;
 
 use combiner::Combine;
 use emitter::IntermediateVecEmitter;
 use errors::*;
+use intermediate::IntermediateInputKV;
 use io::*;
 use mapper::Map;
 use partition::{Partition, PartitionInputPairs};
@@ -51,9 +54,8 @@ where
 {
     match matches.subcommand_name() {
         Some("map") => {
-            run_map(registry.mapper, registry.partitioner).chain_err(
-                || "Error running map",
-            )?;
+            run_map(registry.mapper, registry.partitioner, registry.combiner)
+                .chain_err(|| "Error running map")?;
             Ok(())
         }
         Some("reduce") => {
@@ -85,10 +87,11 @@ where
     }
 }
 
-fn run_map<M, P>(mapper: &M, partitioner: &P) -> Result<()>
+fn run_map<M, P, C>(mapper: &M, partitioner: &P, combiner_option: Option<&C>) -> Result<()>
 where
     M: Map,
     P: Partition<M::Key, M::Value>,
+    C: Combine<M::Key, M::Value>,
 {
     let mut source = stdin();
     let mut sink = stdout();
@@ -101,6 +104,14 @@ where
     mapper
         .map(input_kv, IntermediateVecEmitter::new(&mut pairs_vec))
         .chain_err(|| "Error running map operation.")?;
+
+    if let Some(combiner) = combiner_option {
+        let new_pairs_vec = run_internal_combine(combiner, &mut pairs_vec).chain_err(
+            || "Error running combine on map results",
+        )?;
+
+        pairs_vec = new_pairs_vec;
+    }
 
     let mut output_object = IntermediateOutputObject::<M::Key, M::Value>::default();
 
@@ -159,6 +170,64 @@ where
     write_intermediate_vector(&mut sink, &output_object)
         .chain_err(|| "Error writing combine output to stdout.")?;
     Ok(())
+}
+
+fn run_internal_combine<K, V, C>(combiner: &C, pairs: &mut Vec<(K, V)>) -> Result<Vec<(K, V)>>
+where
+    K: Default + Serialize + DeserializeOwned,
+    V: Default + Serialize + DeserializeOwned,
+    C: Combine<K, V>,
+{
+    let mut kv_map: HashMap<String, Vec<V>> = HashMap::new();
+
+    for pair in pairs.drain(0..) {
+        let mut key = pair.0;
+        let mut value = pair.1;
+
+        // Use serde_json to serialize the key to a string.
+        let key_str = json!(key).to_string();
+
+        let vec = kv_map.entry(key_str).or_insert_with(Vec::new);
+        vec.push(value);
+    }
+
+    let mut results = Vec::new();
+
+    for (key_str, mut values) in kv_map.drain() {
+        let key_json: serde_json::Value = serde_json::from_str(&key_str).chain_err(
+            || "Error parsing combine key.",
+        )?;
+
+        // Retrieve the original key from the serialized version.
+        let key = serde_json::from_value(key_json.clone()).chain_err(
+            || "Error converting combine key string to key type",
+        )?;
+
+        if values.len() > 1 {
+            let input_kv = IntermediateInputKV {
+                key: key,
+                values: values,
+            };
+
+            let mut result_values = Vec::<V>::new();
+
+            combiner
+                .combine(input_kv, VecEmitter::new(&mut result_values))
+                .chain_err(|| "Error running combine operation.")?;
+
+            for value in result_values {
+                let key = serde_json::from_value(key_json.clone()).chain_err(
+                    || "Error converting combine key string to key type",
+                )?;
+
+                results.push((key, value));
+            }
+        } else if values.len() == 1 {
+            results.push((key, values.remove(0)));
+        }
+    }
+
+    Ok(results)
 }
 
 fn run_has_combine<K, V, C>(combiner: Option<&C>)
